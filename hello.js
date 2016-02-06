f5e6c58b7e89b0c2aa662cc6c6496e0fb691697e
Stephen Salzer <stephen.salzer@gmail.com>
2016-02-05 21:07:10 -0800

    second commit

--- hello.js
+++ hello.js
@@ -1,6 +1,17 @@
+// Commented code will not pass
+
+//var http = require('http');
+//http.createServer(function (req, res) {
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  //res.end('Hello Travis!\n') // this will FAIL travis ci lint
+//}).listen(1337, '127.0.0.1');
+//console.log('Server running at http://127.0.0.1:1337');
+
+// Correct code to pass build
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n'); // this will FAIL travis ci lint
+  res.end('Hello Travis!\n'); // build should pass now!
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337');
