ff4c3a4b9c7832cee2eb63fd0e1b6392a4138fc3
Stephen Salzer <stephen.salzer@gmail.com>
2016-02-09 11:01:44 -0800

    make it fail

--- hello.js
+++ hello.js
@@ -1,17 +1,17 @@
-// Commented code will not pass
-
-//var http = require('http');
-//http.createServer(function (req, res) {
-  //res.writeHead(200, {'Content-Type': 'text/plain'});
-  //res.end('Hello World!\n') // this will FAIL travis ci lint
-//}).listen(1337, '127.0.0.1');
-//console.log('Server running at http://127.0.0.1:1337');
-
-// Correct code to pass build
+//Commented code will not pass
 
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!\n'); // build should pass now!
+  res.end('Hello World!\n') // this will FAIL travis ci lint
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337');
+
+// Correct code to pass build
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World!\n'); // build should pass now!
+// }).listen(1337, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:1337');
