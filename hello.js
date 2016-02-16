34b2bf73eded53d4f2f8b5bd7c7ecd4bf324bed9
Stephen Salzer <stephen.salzer@gmail.com>
2016-02-16 12:54:39 -0800

    example of breaking shit

--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!\n'); // This build will pass, it has the semicolon
+  res.end('Hello World!\n') // This build will fail after the push, it is missing a semicolon
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337');
